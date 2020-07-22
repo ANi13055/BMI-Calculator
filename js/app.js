110e7d284449decbd7c5171c49d4acf310e31b54
ANi13055 <techhub13055@gmail.com>
2020-07-22 21:54:16 +0530

    commit via VS Code

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// Variables
+let poundValue = document.querySelector('.pound-value');
+let gramValue = document.querySelector('.gram-value');
+let ounceValue = document.querySelector('.ounce-value');
+let weightBox = document.querySelectorAll('.weight-box')
+let redLabel = document.querySelector('.label');
+
+// TODO: Convert the value onclick
+const convert = document.querySelector('.convert-btn');
+convert.addEventListener('click', function () {
+    let weightValue = document.querySelector('.kg-value').value;
+    let regEx = /\d/;
+
+    if (regEx.test(weightValue)) {
+
+        poundValue.innerText = weightValue * 2.205;
+        gramValue.innerText = weightValue * 1000;
+        ounceValue.innerText = weightValue * 35.274;
+
+        for (i = 0; i < weightBox.length; i++) {
+            weightBox[i].style.opacity = '1';
+        }
+
+        redLabel.style.opacity = '0';
+    }
+    else {
+        for (i = 0; i < weightBox.length; i++) {
+            weightBox[i].style.opacity = '';
+        }
+
+        document.querySelector(".kg-value").value = "";
+
+        redLabel.style.opacity = '1';
+    }
+});
