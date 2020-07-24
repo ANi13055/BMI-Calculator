070900ebcb8b96985c806f4c7d17bf7bba0d57ec
ANi13055 <techhub13055@gmail.com>
2020-07-24 15:16:54 +0530

    change app.js

--- js/app.js
+++ js/app.js
@@ -1,26 +1,32 @@
 // Variables
-let poundValue = document.querySelector(".pound-value");
-let gramValue = document.querySelector(".gram-value");
-let ounceValue = document.querySelector(".ounce-value");
-let weightBox = document.querySelectorAll(".weight-box");
+let weight = document.querySelector('.kg-value');
+let pound = document.querySelector(".pound-value");
+let gram = document.querySelector(".gram-value");
+let ounce = document.querySelector(".ounce-value");
 let redLabel = document.querySelector(".label");
+let convert = document.querySelector(".convert-btn");
 
-// TODO: Convert the value onclick
-const convert = document.querySelector(".convert-btn");
+// Convert the value
 convert.addEventListener("click", function () {
     let weightValue = document.querySelector(".kg-value").value;
-    let regEx = /\d/;
+    let weightBox = document.querySelectorAll(".weight-box");
+
+    // regex in digit
+    let regEx = /^[\d]/;
 
     if (regEx.test(weightValue)) {
-        poundValue.innerText = weightValue * 2.205;
-        gramValue.innerText = weightValue * 1000;
-        ounceValue.innerText = weightValue * 35.274;
+        pound.innerText = weightValue * 2.205;
+        gram.innerText = weightValue * 1000;
+        ounce.innerText = weightValue * 35.274;
         redLabel.style.opacity = "0";
 
         for (i = 0; i < weightBox.length; i++) {
             weightBox[i].style.opacity = '1';
             weightBox[i].style.transition = 'all 350ms ease-out'
         }
+
+        // change button innertext
+        convert.innerText = 'Converted';
     }
     else {
         document.querySelector(".kg-value").value = "";
@@ -29,5 +35,13 @@ convert.addEventListener("click", function () {
         for (i = 0; i < weightBox.length; i++) {
             weightBox[i].style.opacity = '';
         }
+
+        // change button innertext
+        convert.innerText = 'Convert';
     }
 });
+
+weight.addEventListener('keypress', (e) => {
+    // console.log(e);
+    convert.innerText = 'Convert';
+})
