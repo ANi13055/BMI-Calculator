6eb84a03d688499db7f0f3da7669bf9eee60da52
Kailas <65399312+NakumKailas@users.noreply.github.com>
2020-10-26 20:59:50 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 // Variables
 let weight = document.querySelector('.kg-value');
-let pound = document.querySelector(".pound-value");
-let gram = document.querySelector(".gram-value");
-let ounce = document.querySelector(".ounce-value");
-let redLabel = document.querySelector(".label");
-let convert = document.querySelector(".convert-btn");
+let height = document.querySelector('meter-value');
 
-// Convert the value
-convert.addEventListener("click", function () {
+// weight converter
+let weightConvert = document.querySelector(".weight-btn");
+
+weightConvert.addEventListener("click", function () {
     let weightValue = document.querySelector(".kg-value").value;
     let weightBox = document.querySelectorAll(".weight-box");
+    // weight measure
+    let pound = document.querySelector(".pound-value");
+    let gram = document.querySelector(".gram-value");
+    let ounce = document.querySelector(".ounce-value");
 
     // regex in digit
     let regEx = /^[\d]/;
@@ -18,30 +20,81 @@ convert.addEventListener("click", function () {
         pound.innerText = weightValue * 2.205;
         gram.innerText = weightValue * 1000;
         ounce.innerText = weightValue * 35.274;
+
+        // label
+        let redLabel = document.querySelector(".label");
         redLabel.style.opacity = "0";
 
         for (i = 0; i < weightBox.length; i++) {
-            weightBox[i].style.opacity = '1';
-            weightBox[i].style.transition = 'all 350ms ease-out'
+            weightBox[i].classList.add('show');
         }
 
         // change button innertext
-        convert.innerText = 'Converted';
+        weightConvert.innerText = 'Converted';
     }
     else {
         document.querySelector(".kg-value").value = "";
         redLabel.style.opacity = "1";
 
         for (i = 0; i < weightBox.length; i++) {
-            weightBox[i].style.opacity = '';
+            weightBox[i].classList.remove('show');
         }
 
         // change button innertext
-        convert.innerText = 'Convert';
+        weightConvert.innerText = 'Convert';
     }
 });
 
-weight.addEventListener('keypress', (e) => {
+weight.addEventListener('mousedown', (e) => {
     // console.log(e);
-    convert.innerText = 'Convert';
+    weightConvert.innerText = 'Convert';
+});
+
+
+// height converter
+let heightConvert = document.querySelector(".height-btn");
+
+heightConvert.addEventListener("click", function () {
+    let heightValue = document.querySelector(".meter-value").value;
+    let heightBox = document.querySelectorAll(".height-box");
+    // weight measure
+    let km = document.querySelector(".km-value");
+    let inch = document.querySelector(".inch-value");
+    let foot = document.querySelector(".foot-value");
+
+    // regex in digit
+    let regEx = /^[\d]/;
+
+    if (regEx.test(heightValue)) {
+        km.innerText = heightValue * 0.001;
+        inch.innerText = heightValue * 39.37;
+        foot.innerText = heightValue * 3.28;
+
+        // label
+        let redLabel = document.querySelector(".label");
+        redLabel.style.opacity = "0";
+
+        for (i = 0; i < heightBox.length; i++) {
+            heightBox[i].classList.add('show');
+        }
+
+        // change button innertext
+        heightConvert.innerText = 'Converted';
+    }
+    else {
+        document.querySelector(".meter-value").value = "";
+        redLabel.style.opacity = "1";
+
+        for (i = 0; i < heightBox.length; i++) {
+            heightBox[i].classList.remove('show');
+        }
+
+        // change button innertext
+        heightConvert.innerText = 'Convert';
+    }
+});
+
+height.addEventListener('mousedown', (e) => {
+    // console.log(e);
+    heightConvert.innerText = 'Convert';
 })
